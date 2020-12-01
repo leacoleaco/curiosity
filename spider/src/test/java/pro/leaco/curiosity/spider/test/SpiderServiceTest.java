ce70255772e04175bd49cd4d89c677375d82d3dc
leaco <leaco9012@163.com>
2020-12-01 16:18:52 +0800

    add test

--- spider/src/test/java/pro/leaco/curiosity/spider/test/SpiderServiceTest.java
+++ spider/src/test/java/pro/leaco/curiosity/spider/test/SpiderServiceTest.java
@@ -1,13 +1,16 @@
 package pro.leaco.curiosity.spider.test;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import pro.leaco.curiosity.spider.SpringBootSpiderApplication;
 import pro.leaco.curiosity.spider.service.SpiderService;
 
 import javax.annotation.Resource;
 
 @SpringBootTest(classes = SpringBootSpiderApplication.class)
+@RunWith(SpringRunner.class)
 public class SpiderServiceTest {
 
     @Resource
