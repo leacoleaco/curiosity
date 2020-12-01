fd3f8be0204c3326e166d209974c5c49fadc611e
leaco <leaco9012@163.com>
2020-12-01 16:16:04 +0800

    add web support

--- spider/src/main/java/pro/leaco/curiosity/spider/SpringBootSpiderApplication.java
+++ spider/src/main/java/pro/leaco/curiosity/spider/SpringBootSpiderApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("pro.leaco.curiosity.db.**.dao")
-@SpringBootApplication (scanBasePackages = {"pro.leaco.curiosity.spider.service"})
+@SpringBootApplication (scanBasePackages = {
+        "pro.leaco.curiosity.spider.service",
+        "pro.leaco.curiosity.spider.controller",
+})
 public class SpringBootSpiderApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSpiderApplication.class, args);
